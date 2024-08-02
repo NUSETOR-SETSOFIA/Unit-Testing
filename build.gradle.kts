5abea3287f7cad1110de81d8438e777c436b4b81
NUSETOR SETSOFIA KOJO <setsofia.nusetor@amalitech.com>
2024-08-02 10:44:18 +0000

    environment setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ version = "0.0.1-SNAPSHOT"
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
+		languageVersion = JavaLanguageVersion.of(21)
 	}
 }
 
@@ -38,6 +38,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testImplementation("junit:junit:4.13.2")
 }
 
 kotlin {
